ee3f7ccec1367213a234090c4f9789e50ddea089
candytaco <t.zhang@berkeley.edu>
2026-05-27 11:21:00 -0700

    add menu button and motion-energy tab to main window

--- SharpEyes/ViewModels/MainWindowViewModel.cs
+++ SharpEyes/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Reactive;
 using ReactiveUI;
+using SharpEyes.Views;
 
 namespace SharpEyes.ViewModels
 {
@@ -11,6 +10,9 @@ namespace SharpEyes.ViewModels
 		public StimulusGazeViewModel stimulusGazeViewModel { get; }
 		public CalibrationViewModel calibrationViewModel { get; }
 		public RecenteringViewModel recenteringViewModel { get; }
+		public MotionEnergyViewModel motionEnergyViewModel { get; }
+
+		public ReactiveCommand<Unit, Unit>? OpenSettingsCommand { get; } = null;
 
 		private int _selectedTabIndex = 2;
 		public int SelectedTabIndex
@@ -25,8 +27,17 @@ namespace SharpEyes.ViewModels
 			stimulusGazeViewModel = new StimulusGazeViewModel();
 			calibrationViewModel = new CalibrationViewModel();
 			recenteringViewModel = new RecenteringViewModel();
+			motionEnergyViewModel = new MotionEnergyViewModel();
 			stimulusGazeViewModel.RecenteringViewModel = recenteringViewModel;
 			stimulusGazeViewModel.SwitchToRecenteringTab = () => SelectedTabIndex = 3;
+
+			OpenSettingsCommand = ReactiveCommand.Create(() =>
+			{
+				SettingsWindow settingsWindow = new SettingsWindow();
+				settingsWindow.ShowDialog(MainWindow);
+			});
+
+			SharpEyes.Models.PythonEnvironmentManager.Instance.LoadSettings();
 		}
 	}
 }
