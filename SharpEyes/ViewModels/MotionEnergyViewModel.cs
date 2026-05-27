d3545ad4f0e250f71faea583e7160c1c97460300
candytaco <t.zhang@berkeley.edu>
2026-05-27 12:39:18 -0700

    add sidebar items to motion-energy tab

--- SharpEyes/ViewModels/MotionEnergyViewModel.cs
+++ SharpEyes/ViewModels/MotionEnergyViewModel.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.ObjectModel;
 using System.Reactive;
 using Avalonia.Media.Imaging;
 using Avalonia.Threading;
+using NumSharp;
 using ReactiveUI;
 using SharpEyes.Models;
 
@@ -42,15 +44,179 @@ namespace SharpEyes.ViewModels
 		public int VideoWidth
 		{
 			get => _videoWidth;
-			set => this.RaiseAndSetIfChanged(ref _videoWidth, value);
+			set
+			{
+				this.RaiseAndSetIfChanged(ref _videoWidth, value);
+				this.RaisePropertyChanged("RecenteringCanvasWidth");
+				this.RaisePropertyChanged("RecenteringImageLeft");
+				this.RaisePropertyChanged("OutputFrameSizeText");
+				this.RaisePropertyChanged("VideoFrameSizeText");
+			}
 		}
 
 		private int _videoHeight = 768;
 		public int VideoHeight
 		{
 			get => _videoHeight;
-			set => this.RaiseAndSetIfChanged(ref _videoHeight, value);
+			set
+			{
+				this.RaiseAndSetIfChanged(ref _videoHeight, value);
+				this.RaisePropertyChanged("RecenteringCanvasHeight");
+				this.RaisePropertyChanged("RecenteringImageTop");
+				this.RaisePropertyChanged("OutputFrameSizeText");
+				this.RaisePropertyChanged("VideoFrameSizeText");
+			}
 		}
+
+		// == Gaze / recentering (populated via LoadFromRecentering) ==
+
+		private NDArray? _gazeLocations = null;
+		private int? _dataStartFrame = null;
+		private int _eyetrackingFPS = 60;
+		private int _gazeSpaceWidth = 1024;
+		private int _gazeSpaceHeight = 768;
+
+		private bool _isLoadedFromRecentering = false;
+		public bool IsLoadedFromRecentering
+		{
+			get => _isLoadedFromRecentering;
+			private set => this.RaiseAndSetIfChanged(ref _isLoadedFromRecentering, value);
+		}
+
+		private double _gazeX = 0;
+		public double GazeX
+		{
+			get => _gazeX;
+			set
+			{
+				this.RaiseAndSetIfChanged(ref _gazeX, value);
+				this.RaisePropertyChanged("RecenteringImageLeft");
+			}
+		}
+
+		private double _gazeY = 0;
+		public double GazeY
+		{
+			get => _gazeY;
+			set
+			{
+				this.RaiseAndSetIfChanged(ref _gazeY, value);
+				this.RaisePropertyChanged("RecenteringImageTop");
+			}
+		}
+
+		public int RecenteringCanvasWidth => (int)(VideoWidth * _padPercent / 100.0);
+		public int RecenteringCanvasHeight => (int)(VideoHeight * _padPercent / 100.0);
+		public double RecenteringImageLeft => VideoWidth * _padPercent / 100.0 / 2.0 - GazeX * (double)VideoWidth / _gazeSpaceWidth;
+		public double RecenteringImageTop  => VideoHeight * _padPercent / 100.0 / 2.0 - GazeY * (double)VideoHeight / _gazeSpaceHeight;
+
+		// == Video output parameters ==
+
+		private double _padPercent = 200;
+		public double PadPercent
+		{
+			get => _padPercent;
+			set
+			{
+				this.RaiseAndSetIfChanged(ref _padPercent, value);
+				this.RaisePropertyChanged("RecenteringCanvasWidth");
+				this.RaisePropertyChanged("RecenteringCanvasHeight");
+				this.RaisePropertyChanged("RecenteringImageLeft");
+				this.RaisePropertyChanged("RecenteringImageTop");
+				this.RaisePropertyChanged("OutputFrameSizeText");
+			}
+		}
+
+		private double _padValue = 0.1;
+		public double PadValue
+		{
+			get => _padValue;
+			set => this.RaiseAndSetIfChanged(ref _padValue, value);
+		}
+
+		private double _frameScale = 0.125;
+		public double FrameScale
+		{
+			get => _frameScale;
+			set
+			{
+				this.RaiseAndSetIfChanged(ref _frameScale, value);
+				this.RaisePropertyChanged("OutputFrameSizeText");
+				this.RaisePropertyChanged("VideoFrameSizeText");
+			}
+		}
+
+		public string OutputFrameSizeText => String.Format("{0} x {1}",
+			(int)(VideoWidth * _padPercent / 100.0 * _frameScale),
+			(int)(VideoHeight * _padPercent / 100.0 * _frameScale));
+		public string VideoFrameSizeText => String.Format("{0} x {1}",
+			(int)(VideoWidth * _frameScale),
+			(int)(VideoHeight * _frameScale));
+
+		// == Motion-energy pyramid parameters ==
+
+		private double _videoFps = 30;
+		public double VideoFps
+		{
+			get => _videoFps;
+			set => this.RaiseAndSetIfChanged(ref _videoFps, value);
+		}
+
+		public ObservableCollection<double> SpatialFrequencies { get; } =
+			new ObservableCollection<double> { 0, 2, 4, 8, 16, 32 };
+		public ObservableCollection<double> TemporalFrequencies { get; } =
+			new ObservableCollection<double> { 0, 2, 4, 8, 16 };
+		public ObservableCollection<double> Directions { get; } =
+			new ObservableCollection<double> { 0, 45, 90, 135, 180, 225, 270, 315 };
+
+		private int _selectedSpatialFrequencyIndex = -1;
+		public int SelectedSpatialFrequencyIndex
+		{
+			get => _selectedSpatialFrequencyIndex;
+			set => this.RaiseAndSetIfChanged(ref _selectedSpatialFrequencyIndex, value);
+		}
+
+		private int _selectedTemporalFrequencyIndex = -1;
+		public int SelectedTemporalFrequencyIndex
+		{
+			get => _selectedTemporalFrequencyIndex;
+			set => this.RaiseAndSetIfChanged(ref _selectedTemporalFrequencyIndex, value);
+		}
+
+		private int _selectedDirectionIndex = -1;
+		public int SelectedDirectionIndex
+		{
+			get => _selectedDirectionIndex;
+			set => this.RaiseAndSetIfChanged(ref _selectedDirectionIndex, value);
+		}
+
+		private double _newSpatialFrequency = 0;
+		public double NewSpatialFrequency
+		{
+			get => _newSpatialFrequency;
+			set => this.RaiseAndSetIfChanged(ref _newSpatialFrequency, value);
+		}
+
+		private double _newTemporalFrequency = 0;
+		public double NewTemporalFrequency
+		{
+			get => _newTemporalFrequency;
+			set => this.RaiseAndSetIfChanged(ref _newTemporalFrequency, value);
+		}
+
+		private double _newDirection = 0;
+		public double NewDirection
+		{
+			get => _newDirection;
+			set => this.RaiseAndSetIfChanged(ref _newDirection, value);
+		}
+
+		public ReactiveCommand<Unit, Unit> AddSpatialFrequencyCommand { get; }
+		public ReactiveCommand<Unit, Unit> RemoveSpatialFrequencyCommand { get; }
+		public ReactiveCommand<Unit, Unit> AddTemporalFrequencyCommand { get; }
+		public ReactiveCommand<Unit, Unit> RemoveTemporalFrequencyCommand { get; }
+		public ReactiveCommand<Unit, Unit> AddDirectionCommand { get; }
+		public ReactiveCommand<Unit, Unit> RemoveDirectionCommand { get; }
 
 		private Bitmap? _videoFrame = null;
 		public Bitmap? VideoFrame
@@ -109,6 +275,44 @@ namespace SharpEyes.ViewModels
 			NextFrameCommand = ReactiveCommand.Create(() => { ChangeFrame(1); });
 			_videoPlaybackTimer = new DispatcherTimer();
 			_videoPlaybackTimer.Tick += VideoTimerTick;
+			AddSpatialFrequencyCommand = ReactiveCommand.Create(() => SpatialFrequencies.Add(_newSpatialFrequency));
+			RemoveSpatialFrequencyCommand = ReactiveCommand.Create(() =>
+			{
+				if (_selectedSpatialFrequencyIndex >= 0 && _selectedSpatialFrequencyIndex < SpatialFrequencies.Count)
+					SpatialFrequencies.RemoveAt(_selectedSpatialFrequencyIndex);
+			});
+			AddTemporalFrequencyCommand = ReactiveCommand.Create(() => TemporalFrequencies.Add(_newTemporalFrequency));
+			RemoveTemporalFrequencyCommand = ReactiveCommand.Create(() =>
+			{
+				if (_selectedTemporalFrequencyIndex >= 0 && _selectedTemporalFrequencyIndex < TemporalFrequencies.Count)
+					TemporalFrequencies.RemoveAt(_selectedTemporalFrequencyIndex);
+			});
+			AddDirectionCommand = ReactiveCommand.Create(() => Directions.Add(_newDirection));
+			RemoveDirectionCommand = ReactiveCommand.Create(() =>
+			{
+				if (_selectedDirectionIndex >= 0 && _selectedDirectionIndex < Directions.Count)
+					Directions.RemoveAt(_selectedDirectionIndex);
+			});
+		}
+
+		public void LoadFromRecentering(VideoReader videoReader, NDArray gazeLocations, int dataStartFrame, int eyetrackingFPS, int gazeSpaceWidth, int gazeSpaceHeight)
+		{
+			if (IsVideoPlaying) PlayPause();
+			_videoReader = videoReader;
+			_gazeLocations = gazeLocations;
+			_dataStartFrame = dataStartFrame;
+			_eyetrackingFPS = eyetrackingFPS;
+			_gazeSpaceWidth = gazeSpaceWidth;
+			_gazeSpaceHeight = gazeSpaceHeight;
+			VideoWidth = videoReader.width;
+			VideoHeight = videoReader.height;
+			_videoPlaybackTimer.Interval = TimeSpan.FromMilliseconds(1000.0 / (double)videoReader.fps);
+			TotalVideoFrames = videoReader.frameCount;
+			TotalVideoTime = videoReader.FramesToTimecode(videoReader.frameCount - 1);
+			VideoFps = videoReader.fps;
+			IsLoadedFromRecentering = true;
+			this.RaisePropertyChanged("CanPlayVideo");
+			UpdateDisplay();
 		}
 
 		public async void LoadVideo()
@@ -165,11 +369,30 @@ namespace SharpEyes.ViewModels
 			UpdateDisplay();
 		}
 
+		private int VideoTimeToDataIndex(int videoFrame)
+		{
+			int videoFramesElapsed = videoFrame - _dataStartFrame.Value;
+			if (videoFramesElapsed < 0) return 0;
+			double videoElapsedTime = (double)videoFramesElapsed / _videoReader.fps;
+			return (int)(videoElapsedTime * _eyetrackingFPS);
+		}
+
 		public void UpdateDisplay()
 		{
 			VideoFrame = _videoReader.GetFrameForDisplay();
 			CurrentVideoFrame = _videoReader.CurrentFrameNumber;
 			CurrentVideoTime = _videoReader.FramesToTimecode(_videoReader.CurrentFrameNumber);
+			if ((object)_gazeLocations != null && _dataStartFrame != null)
+			{
+				int dataIndex = Math.Clamp(VideoTimeToDataIndex(CurrentVideoFrame), 0, _gazeLocations.Shape[0] - 1);
+				double gazeXValue = (double)_gazeLocations[dataIndex, 0];
+				double gazeYValue = (double)_gazeLocations[dataIndex, 1];
+				if (!Double.IsNaN(gazeXValue) && !Double.IsNaN(gazeYValue))
+				{
+					GazeX = gazeXValue;
+					GazeY = gazeYValue;
+				}
+			}
 		}
 	}
 }
